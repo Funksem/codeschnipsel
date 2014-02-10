d4883207f3410d395ec9d1774731dad17755408b
tom_h_ <holzhaus16@googlemail.com>
2014-02-10 17:15:46 +0100

    initial version

---
+++ src/main/java/de/funksem/codeschnipsel/App.java
@@ -0,0 +1,13 @@
+package de.funksem.codeschnipsel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
