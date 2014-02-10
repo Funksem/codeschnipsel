9487cb5f9fb282da0c852ca577ba2e9c6a1f373e
tom_h_ <holzhaus16@googlemail.com>
2014-02-10 17:20:49 +0100

    xxx

--- src/main/java/de/funksem/codeschnipsel/App.java
+++ src/main/java/de/funksem/codeschnipsel/App.java
@@ -2,12 +2,12 @@ package de.funksem.codeschnipsel;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello Tom!");
     }
 }
